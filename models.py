0def010b492f11b626b45fa251da34d4936a4cec
Dominik <101473446+b14ucky@users.noreply.github.com>
2024-01-11 19:40:09 +0100

    refactor: rewrite some number recognition trainer features to make more sense

--- models.py
+++ models.py
@@ -5,9 +5,9 @@ import torch.optim as optim
 import matplotlib.pyplot as plt
 
 
-class NumberRecognitionNet(nn.Module):
+class NumberRecognitionModel(nn.Module):
     def __init__(self):
-        super(NumberRecognitionNet, self).__init__()
+        super(NumberRecognitionModel, self).__init__()
         self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
         self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
         self.conv2_drop = nn.Dropout2d()
@@ -24,56 +24,70 @@ class NumberRecognitionNet(nn.Module):
         return F.log_softmax(x, dim=1)
 
 
-class NumberRecognitionModel:
-    def __init__(self, lr=0.01, momentum=0.5):
-        self.net = NumberRecognitionNet()
-        self.optimizer = optim.SGD(self.net.parameters(), lr=lr, momentum=momentum)
+class NumberRecognitionTrainer:
+    def __init__(self, train_loader, test_loader, lr=0.01, momentum=0.5):
+        self.model = NumberRecognitionModel()
+        self.train_loader = train_loader
+        self.test_loader = test_loader
+
+        self.lr = lr
+        self.momentum = momentum
+        self.optimizer = optim.SGD(
+            self.model.parameters(), lr=self.lr, momentum=self.momentum
+        )
+
+        # data for plotting
         self.train_counter = []
         self.train_losses = []
         self.test_counter = []
         self.test_losses = []
 
-    def train(self, train_loader, test_loader, epochs=10, log_interval=10):
-        self.test_counter = [i * len(train_loader.dataset) for i in range(epochs + 1)]
-        self.accuracy(test_loader)
+    def train(self, epochs=10, log_interval=10):
+        self.test_counter = [
+            i * len(self.train_loader.dataset) for i in range(epochs + 1)
+        ]
+
+        self.accuracy()
+
         for epoch in range(epochs):
-            self.net.train()
-            for batch_idx, (data, target) in enumerate(train_loader):
+            self.model.train()
+            for batch_idx, (data, target) in enumerate(self.train_loader):
                 self.optimizer.zero_grad()
-                output = self.net(data)
+                output = self.model(data)
                 loss = F.nll_loss(output, target)
                 loss.backward()
                 self.optimizer.step()
 
                 if batch_idx % log_interval == 0:
                     print(
-                        f"Epoch: {epoch + 1} / {epochs} [{batch_idx * len(data)}/{len(train_loader.dataset)} ({100.0 * batch_idx / len(train_loader):.0f}%)]\tLoss: {loss.item():.6f}"
+                        f"Epoch: {epoch + 1} / {epochs} [{batch_idx * len(data)}/{len(self.train_loader.dataset)} ({100.0 * batch_idx / len(self.train_loader):.0f}%)]\tLoss: {loss.item():.6f}"
                     )
 
                     self.train_losses.append(loss.item())
                     self.train_counter.append(
-                        (batch_idx * 64) + ((epoch - 1) * len(train_loader.dataset))
+                        (batch_idx * 64)
+                        + ((epoch - 1) * len(self.train_loader.dataset))
                     )
 
-            self.accuracy(test_loader)
+            self.accuracy()
 
         print("Finished Training")
 
-    def accuracy(self, test_loader):
-        self.net.eval()
+    def accuracy(self):
+        self.model.eval()
         test_loss = 0
         correct = 0
         with torch.no_grad():
-            for data, target in test_loader:
-                output = self.net(data)
+            for data, target in self.test_loader:
+                output = self.model(data)
                 test_loss += F.nll_loss(output, target, reduction="sum").item()
                 prediction = output.data.max(1, keepdim=True)[1]
                 correct += prediction.eq(target.data.view_as(prediction)).sum()
 
-        test_loss /= len(test_loader.dataset)
+        test_loss /= len(self.test_loader.dataset)
         self.test_losses.append(test_loss)
         print(
-            f"Test set: Avg. loss: {test_loss:.4f}, Accuracy: {correct}/{len(test_loader.dataset)} ({100.0 * correct / len(test_loader.dataset):.0f}%)"
+            f"Test set: Avg. loss: {test_loss:.4f}, Accuracy: {correct}/{len(self.test_loader.dataset)} ({100.0 * correct / len(self.test_loader.dataset):.0f}%)"
         )
 
     def test_prediction(
@@ -83,7 +97,7 @@ class NumberRecognitionModel:
             wrong = 0
             with torch.no_grad():
                 for i in range(number_of_predictions):
-                    output = self.net(prediction_data[i].view(1, 1, 28, 28))
+                    output = self.model(prediction_data[i].view(1, 1, 28, 28))
                     _, predicted = torch.max(output.data, 1)
                     if predicted[0] != prediction_target[i].item():
                         print(
@@ -95,15 +109,17 @@ class NumberRecognitionModel:
 
     def predict(self, data):
         with torch.no_grad():
-            output = self.net(data.view(1, 1, 28, 28))
+            output = self.model(data.view(1, 1, 28, 28))
             _, predicted = torch.max(output.data, 1)
             return predicted[0].item()
 
-    def save(self, path):
-        torch.save(self.net.state_dict(), path)
+    def save(self, file_name="NRmodel"):
+        torch.save(self.model.state_dict(), f"{file_name}.pth")
+        torch.save(self.optimizer.state_dict(), f"{file_name}-optimizer.pth")
 
-    def load(self, path):
-        self.net.load_state_dict(torch.load(path))
+    def load(self, file_name="NRmodel"):
+        self.model.load_state_dict(torch.load(file_name))
+        self.optimizer.load_state_dict(torch.load(f"{file_name}-optimizer.pth"))
 
     def plot(self):
         plt.plot(self.train_counter, self.train_losses, color="blue")
